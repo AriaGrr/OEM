b7e11d3011222508316515dd8166f2a2993f9ae6
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-03-11 00:43:38 +0000

    limpando

--- main.py
+++ main.py
@@ -13,23 +13,17 @@
 # Retorno de valores em 4 significativos
 
 # Bibliotecas
-#import decimal
 import math
 from decimal import Decimal
 
 # Variaveis globais
-
-teste = Decimal(0)
+teste = Decimal(0) # Variável para testar se é a primeira vez que o programa é executado
 
 # Constantes
-#pi = Decimal(3.14159265359) # Número pi
 pi = Decimal(3.1415) # Número pi com 4 significativos
 c = Decimal(299792458) # Velocidade da luz no vácuo em m/s
-#c = Decimal(3 * 10**8) # Velocidade da luz no vácuo em m/s
-#u = Decimal(4 * pi * 10**-7) # Permeabilidade magnética do vácuo em H/m
 u = Decimal(4) * pi * Decimal('1e-7') # Permeabilidade magnética do vácuo em H/m
 e = Decimal(8.854187817 * 10**-12) # Permissividade elétrica do vácuo em F/m
-#dois = Decimal(2) # Número 2, pelo bug do Decimal
 
 # Variáveis
 f = Decimal(0) # Frequência da onda em Hz
@@ -54,27 +48,11 @@ def limpar_variaveis():
     Bm = Decimal(0)
     I = Decimal(0)
 
-# Função para verificar se o valor é um número
-#def is_number(s):
-    #try:
-        #float(s)
-        #return True
-    #except ValueError:
-        #return False
-
-# Corrigir as funções de calculo abaixo que não estão corretas.
-
 # Função para calcular frequencia a partir da frequência angular
 # Correta
 def frequencia_frequencia_angular():
     f = w / (2 * pi)
     return f
-
-# Função para calcular o comprimento de onda apartir da frequência angular
-# 
-#def comprimento_onda_frequencia_angular():
-    #l = 2 * pi / w
-    #return l
 
 # Função para calcular a frequência angular
 # Correta
@@ -124,14 +102,12 @@ def amplitude_campo_eletrico():
 # Função para calcular a intensidade da onda
 # Correta
 def intensidade_onda():
-    #I = (Em * Bm) / (2 * c)
     I = Decimal(1/2) * e * c * Em**2
     return I
 
 # Função para calcular a amplitude do campo magnético a partir da amplitude do campo elétrico
 # Correta
 def amplitude_campo_magnetico():
-    #Bm = Em / (c * u)
     Bm = Em / c
     return Bm
 
@@ -162,13 +138,13 @@ def elevar():
 def conversores():
     clear_screen()
     print('Opções de conversão:')
-    #Correta
+    # Correta
     print('1 - Microtesla para Tesla (ut -> T)')
     #
     print('2 - Tesla para Microtesla (T -> ut)')
-    #
+    # Correta
     print('3 - E')
-    #
+
     print('0 - Voltar!')
     option = input('Escolha uma opção: ')
 
@@ -176,20 +152,12 @@ def conversores():
         print('Opção 1 selecionada...')
         print('Digite o valor em microtesla (ut):')
         ut = Decimal(input('ut: '))
-        #if not is_number(ut):
-            #print('Valor inválido. Tente novamente.')
-            #return
-        #ut = Decimal(round(float(ut), 4))
         T = ut_para_t(ut)
         print(f'{ut} ut = {T:.4g} T')
     elif option == '2':
         print('Opção 2 selecionada...')
         print('Digite o valor em Tesla (T):')
         T = Decimal(input('T: '))
-        #if not is_number(T):
-            #print('Valor inválido. Tente novamente.')
-            #return
-        #T = Decimal(round(float(T), 4))
         ut = t_para_ut(T)
         print(f'{T} T = {ut:.4g} ut')
     elif option == '3':
@@ -223,38 +191,22 @@ def menu():
     # Correta
     print('5 - Comprimento de onda (lambda) em m:')
     print('Retorna f, k e w.')
-    # 
+    # Correta
     print('6 - Número de onda (k) em rad/m:')
     print('Retorna f, lambda e w.')
-    #
+    # Correta
     print('7 - Frequencia angular (w) em rad/s:')
     print('Retorna f, lambda e k.')
-    #
+
     print('0 - Voltar!')
     option = input('Escolha uma opção: ')
 
     if option == '1':
         print('Opção 1 selecionada...')
-        #print('O valor da amplitude do campo elétrico (Em) está em V/m?')
-        #print('s/n')
-        #option = input('Escolha uma opção: ')
-        #if option == 's':
-
         print('Digite a amplitude do campo elétrico (Em) em V/m:')
         Em = Decimal(input('Em: '))
-        #if not is_number(Em):
-            #print('Valor inválido. Tente novamente.')
-            #return
-        #Em = Decimal(round(float(Em), 4))
-
-        #Bm = Decimal(round(amplitude_campo_magnetico(), 4))
         Bm = amplitude_campo_magnetico()
         I = intensidade_onda()
-        # Teste 
-        #print(Em)
-        #print(Bm)
-        #print(I)
-        #
         print(f'Amplitude do campo magnético (Bm): {Bm:.4g} T')
         print(f'Intensidade da onda (I): {I:.4g} W/m^2')
         limpar_variaveis()
@@ -267,10 +219,6 @@ def menu():
         if option == 's':
             print('Digite a amplitude do campo magnético (Bm) em T:')
             Bm = Decimal(input('Bm: '))
-            #if not is_number(Bm):
-                #print('Valor inválido. Tente novamente.')
-                #return
-            #Bm = Decimal(round(float(Bm), 4))
             Em = amplitude_campo_eletrico()
             I = intensidade_onda()
             print(f'Amplitude do campo elétrico (Em): {Em:.4g} V/m')
@@ -284,16 +232,9 @@ def menu():
                 print('Opção 1 selecionada...')
                 print('Digite a amplitude do campo magnético (Bm) em ut:')
                 Bm = Decimal(input('Bm: '))
-                #if not is_number(Bm):
-                    #print('Valor inválido. Tente novamente.')
-                    #return
-                #Bm = Decimal(round(float(Bm), 4))
                 Bm = ut_para_t(Bm)
                 Em = amplitude_campo_eletrico()
                 I = intensidade_onda()
-                #print(Bm)
-                #print(Em)
-                #print(I)
                 print(f'Amplitude do campo elétrico (Em): {Em:.4g} V/m')
                 print(f'Intensidade da onda (I): {I:.4g} W/m^2')
             elif option == '0':
@@ -307,10 +248,6 @@ def menu():
         print('Opção 3 selecionada...')
         print('Digite a intensidade da onda (I) em W/m^2:')
         I = Decimal(input('I: '))
-        #if not is_number(I):
-            #print('Valor inválido. Tente novamente.')
-            #return
-        #I = Decimal(round(float(I), 4))
         Em = amplitude_campo_eletrico_intensidade()
         Bm = amplitude_campo_magnetico()
         print(f'Amplitude do campo elétrico (Em): {Em:.4g} V/m')
@@ -334,10 +271,6 @@ def menu():
         elif option == 'n':
             print('Digite a frequência da onda (f) em Hz:')
             f = Decimal(input('f: '))
-            #if not is_number(f):
-                #print('Valor inválido. Tente novamente.')
-                #return
-            #f = Decimal(round(float(f), 4))
             l = comprimento_onda()
             k = numero_onda()
             w = frequencia_angular()
@@ -364,10 +297,6 @@ def menu():
         elif option == 'n':
             print('Digite o comprimento de onda (lambda) em m:')
             l = Decimal(input('lambda: '))
-            #if not is_number(l):
-                #print('Valor inválido. Tente novamente.')
-                #return
-            #l = Decimal(round(float(l), 4))
             f = frequencia()
             k = numero_onda()
             w = frequencia_angular()
@@ -394,10 +323,6 @@ def menu():
         elif option == 'n':
             print('Digite o número de onda (k) em rad/m:')
             k = Decimal(input('k: '))
-            #if not is_number(k):
-                #print('Valor inválido. Tente novamente.')
-                #return
-            #k = Decimal(round(float(k), 4))
             l = comprimento_numero_onda()
             f = frequencia()
             w = frequencia_angular()
@@ -415,7 +340,6 @@ def menu():
         option = input('Escolha uma opção: ')
         if option == 's':
             w = Decimal(elevar())
-            # Correto
             f = frequencia_frequencia_angular()
             print(f'Frequência da onda (f): {f:.4g} Hz')
             l = comprimento_onda()
@@ -426,10 +350,6 @@ def menu():
         elif option == 'n':
             print('Digite a frequência angular (w) em rad/s:')
             w = Decimal(input('w: '))
-            #if not is_number(w):
-                #print('Valor inválido. Tente novamente.')
-                #return
-            #w = Decimal(round(float(w), 4))
             f = frequencia_frequencia_angular()
             print(f'Frequência da onda (f): {f:.4g} Hz')
             l = comprimento_onda()
